ba035c287792d1b3fe658d2e256ad4e91d687730
Sourav Mandal <sm227465@gmail.com>
2022-09-19 21:40:19 +0530

    array types

---
+++ src/fundamentals/arrays.ts
@@ -0,0 +1,8 @@
+// for number type => array should contain only numbers
+let numbers: number[] = [1, 2, 3];
+
+// string type
+let strings: string[] = ['a', 'b', 'c'];
+
+// object type
+let objects: object[] = [{ name: 'Raizo', age: 25 }];
