200a806752ad46b498d890e34d273ae5aaa8cad8
Sourav Mandal <sm227465@gmail.com>
2022-09-19 21:51:41 +0530

    tuple

---
+++ src/fundamentals/tuples.ts
@@ -0,0 +1,5 @@
+/* TypeScript has a new type call Tuple which is a fixed length array where each element has a particular type, we often use them when working with pair of values
+for example each user we wanna represent two values ID and Name 
+Recomended for 2 values, NOT Recomended if there is more than 2 values */
+
+let user: [number, string] = [1, 'Raizo'];
