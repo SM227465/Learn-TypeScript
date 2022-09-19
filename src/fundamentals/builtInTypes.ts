79d0d0e4a8f8cfc4acad0bcdd856c99b66d77f1f
Sourav Mandal <sm227465@gmail.com>
2022-09-19 21:21:09 +0530

    built in types

---
+++ src/fundamentals/builtInTypes.ts
@@ -0,0 +1,21 @@
+/*
+JavaScript has built-in types like
+-number
+-string
+-boolean
+-null
+-undefined
+-object
+
+TypeScript extend this list and introduces new type like
+-any
+-unknown
+-never
+-enum
+-tuple
+*/
+
+// Primitive types in TypeScript
+let sales: number = 123_456_789;
+let course: string = 'TypeScript';
+let is_published: boolean = true;
