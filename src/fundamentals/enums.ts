1ac8116e177d9128b58090f69e41bdecd83cafac
Sourav Mandal <sm227465@gmail.com>
2022-09-21 21:28:00 +0530

    enum

---
+++ src/fundamentals/enums.ts
@@ -0,0 +1,40 @@
+/* TypeScript has another built-in type call enum which represent a list of related constant,
+lets imagine we wanna represent size of T-shirt as constant */
+
+// one way to define
+/*
+const small = 1
+const medium = 2
+const large = 3
+*/
+
+// another way to represent
+enum Size {
+  Small,
+  Medium,
+  large,
+}
+
+let mySize: Size = Size.Medium;
+
+console.log(mySize);
+
+// by default TypeScript compiler will assign 0 to the first member and others value will be increased by 1 in order and it should look like -
+
+/*
+enum Size {
+  Small = 0,
+  Medium = 1,
+  large = 2,
+}
+*/
+
+// If we dont want thease values we can explicitly set values like what ever we want in order like here we want start from 1 and next 2 and so on
+
+/*
+enum Size {
+  Small = 1,
+  Medium = 2,
+  large = 3,
+}
+*/
