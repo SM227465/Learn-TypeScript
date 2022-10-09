dbfb2467f5e114cc51cab83020c4b31f2a6da02e
Sourav Mandal <sm227465@gmail.com>
2022-10-09 18:12:32 +0530

    literal type

---
+++ src/advancedTypes/literalType.ts
@@ -0,0 +1,22 @@
+/*
+sometime we wanna limit the value we can assign to a varible, This is where we use literal type.
+
+lets declare a variable 'quantity' and assume that quantity can either be 50 or 100 but nothing else
+*/
+
+// let quantity: 50 | 100 = 50
+
+/*
+we can make this code better even better
+
+instead of hard coding this literal values here we can create a custome type using a type aliases
+*/
+
+type Quantity = 50 | 100;
+
+let quantity: Quantity = 100;
+
+// literals dont have to be numbers, They can also be a string
+type Metric = 'cm' | 'inch';
+
+let metric: Metric = 'cm';
