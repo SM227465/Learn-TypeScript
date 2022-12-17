e1f64e30dc268d646306e8e9e1eca0112a4f5a75
Sourav Mandal <sm227465@gmail.com>
2022-12-17 19:05:34 +0530

    decorators

---
+++ src/decorators/introduction.md
@@ -0,0 +1,7 @@
+In this section we gonna talk about decorators which allow us to change and enhance our classes.
+- What are decorators
+- Class decorators
+- Method decorators
+- Property decorators
+- Accessor decorators
+- Parameter decorators
---
+++ src/decorators/whatAreDecorators.ts
@@ -0,0 +1,8 @@
+/*
+What are decorators? => Decorators are attributes that we apply to our classes and their members, and with this we can change how they behave
+*/
+
+// Lets imagne we have a class call ProfileComponent now applying @Component decorator we can convert this class to a component (in Angular)
+
+// @Component
+// class ProfileComponent {}
